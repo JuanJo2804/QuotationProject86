29f9d9be66da6521943ac8298417227863f6091f
Jesus_Landero <landeroj2005@gmail.com>
2025-11-07 13:39:22 -0500

    feat:  se agrego conexion con base de datos para el CRUD del login

--- quotation_project/settings.py
+++ quotation_project/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'quotation_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CRUD_QUOTATION',      
+        'USER': 'postgres',
+        'PASSWORD': '',  
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
