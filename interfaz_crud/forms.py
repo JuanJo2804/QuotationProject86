7d996b30b1ef26dc7ad237df9f8e68dca9dd3c48
Jesus_Landero <landeroj2005@gmail.com>
2025-11-25 18:40:57 -0500

    Se agrega documentación específica para el módulo Filterss y el formulario de filtro de cotizaciones.

--- interfaz_crud/forms.py
+++ interfaz_crud/forms.py
@@ -5,10 +5,11 @@ class ClienteForm(forms.ModelForm):
     """Formulario para crear y editar clientes."""
     class Meta:
         model = Cliente
-        fields = ['nombre', 'correo', 'telefono', 'direccion']
+        fields = ['nombre', 'correo', 'telefono', 'direccion', 'descripcion'] # Añadimos 'descripcion'
         widgets = {
             'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre completo'}),
             'correo': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'correo@ejemplo.com'}),
-            'telefono': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Teléfono'}) ,
+            'telefono': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Teléfono'}),
             'direccion': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Dirección (calle, número, ciudad)'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Descripción adicional...', 'rows': 3}), # Añadimos widget para 'descripcion'
         }
